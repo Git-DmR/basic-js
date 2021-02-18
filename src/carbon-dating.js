66e914285564256b8e558e8981856240873b7225
Git-DmR <dm.r@mail.uk>
2021-02-18 12:57:27 +0300

    feat: carbon-dating solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let value;
+  if (typeof sampleActivity == "string") {
+    value = parseFloat(sampleActivity);
+  } else return false;
+
+  if (value > 0 && value < MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / value) / (0.693 / HALF_LIFE_PERIOD));
+  } else return false;
 };
